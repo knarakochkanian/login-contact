628463062584c1362e2d6ccac66c4a206b948087
Knara Kochkanian <knarak@yandex.ru>
2021-11-03 00:00:46 +0400

    Add all my files

---
+++ src/components/contactform.js
@@ -0,0 +1,82 @@
+import React, { useState} from 'react';
+import MaterialTable from '@material-table/core'
+import { render } from '@testing-library/react';
+import { Avatar } from '@material-ui/core';
+
+const empList = [
+  { id: 1, name: "Anna", email: 'annaj@gmail.com', phone: 9876543210, },
+  { id: 2, name: "Raya", email: 'raya@gmail.com', phone: 9812345678, },
+  { id: 3, name: "David", email: 'david342@gmail.com', phone: 7896536289,},
+  { id: 4, name: "Vika", email: 'vika75@gmail.com', phone: 9087654321,},
+]
+
+function Contactform() {
+
+  const [data, setData] = useState(empList);
+  const columns = [
+    { title: "ID", field: "id", editable: false },
+    { title: "Name", field: "name", render:(row)=><Avatar>{row.name[0]}</Avatar>},
+    { title: "Name", field: "name" },
+    { title: "Phone Number", field: 'phone', },
+    { title: "Email", field: "email" },
+  ]
+
+  return (
+    <div className="App">
+      
+      <MaterialTable
+        title="Data"
+        data={data}
+        columns={columns}
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowDelete: selectedRow => new Promise((resolve, reject) => {
+            const index = selectedRow.tableData.id;
+            const updatedRows = [...data]
+            updatedRows.splice(index, 1)
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
+            const index=oldRow.tableData.id;
+            const updatedRows=[...data]
+            updatedRows[index]=updatedRow
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onBulkUpdate:selectedRows=>new Promise((resolve,reject)=>{
+            const rows=Object.values(selectedRows)
+            const updatedRows=[...data]
+            let index;
+            rows.map (emp => {
+               index=emp.oldData.tableData.id
+               updatedRows[index]=emp.newData
+            })
+            setTimeout(()=>{
+              setData(updatedRows)
+              resolve()
+            },2000)
+          
+          })
+
+        }}
+        options={{
+          actionsColumnIndex: -1, addRowPosition: "first"
+        }}
+      />
+    </div>
+  );
+}
+
+export default Contactform;
+
